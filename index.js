abd793678ec1d119d325a96724f3570a16913a9d
TECOA136_Aditya_ Deo <ahdeo2015@gmail.com>
2022-07-20 23:09:20 +0530

    remove bugs

--- index.js
+++ index.js
@@ -25,11 +25,11 @@ mongoose.connect(
 //Bind connection to error event (to get notification of connection errors)
 // db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-app.use(express.static(path.join(__dirname, "/public")));
+// app.use(express.static(path.join(__dirname, "/public")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public", "index.html"));
+// });
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
